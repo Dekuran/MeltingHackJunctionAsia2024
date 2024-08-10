4bb891a768eb89f619c377d9e8de37a72322b87a
Anti9uA <didlaak6000@naver.com>
2024-08-10 18:02:12 +0900

    feat: onboarding view wip, import Lottie

---
+++ iOS/Mommy/Onboarding/View/RootOnboardingView.swift
@@ -0,0 +1,32 @@
+//
+//  RootOnboardingView.swift
+//  Mommy
+//
+//  Created by Kyubo Shim on 8/10/24.
+//
+
+import SwiftUI
+
+struct RootOnboardingView: View {
+    @State private var currentPage: Int = 0
+    @State private var showEgg = false
+    @State private var showView = true
+    
+    var pages: [AnyView] {
+        [
+            AnyView(OnboardingWelcomeView(currentPage: $currentPage)),
+            AnyView(EnterInfoView(currentPage: $currentPage)),
+        ]
+    }
+    
+    var body: some View {
+        if currentPage < pages.count {
+            pages[currentPage]
+        }
+        
+    }
+}
+
+#Preview {
+    RootOnboardingView()
+}

be4b6a6557e9b76685f4ec8765fa3a9dff4f0976
Anti9uA <didlaak6000@naver.com>
2024-08-10 20:18:01 +0900

    bugfix: onboarding view bugfix

--- iOS/Mommy/Onboarding/View/EnterInfoView.swift
+++ iOS/Mommy/Onboarding/View/EnterInfoView.swift
@@ -16,101 +16,116 @@ struct EnterInfoView: View {
     @State private var allergies = ""
     @Binding var currentPage: Int
     
-    var body: some View {
+    // 모든 필드가 채워졌는지 확인하는 변수
+    private var isFormComplete: Bool {
+        !userName.isEmpty && currentStage != nil && !weekOfPregnancy.isEmpty && !allergies.isEmpty
+    }
     
-        VStack(spacing: 20) {
-            Spacer()
-            Text("Tell us who you are!")
-                .font(.largeTitle)
-                .fontWeight(.semibold)
-                .padding(.bottom, 30)
-            
-            VStack(alignment: .leading, spacing: 15) { 
-                Group {
-                    Text("Enter your full name")
-                    TextField("Name", text: $userName)
-                        .frame(height: 40)
-                        .padding(.horizontal, 15)
-                        .padding(.vertical, 5)
-                        .background(Color.white)
-                        .cornerRadius(15)
-                        .shadow(color: .black.opacity(0.2), radius: 3)
-                        .frame(maxWidth: 350)
-                }
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 20) {
+                Spacer()
                 
-                Group {
-                    Text("Select your current stage")
-                    DropDownView(
-                        hint: "Select",
-                        options: [
-                            "Pre-Pregnant",
-                            "Currently Pregnant",
-                            "Post-Pregnant"
-                        ],
-                        anchor: .bottom,
-                        selection: $currentStage
-                    )
-                }
-                
-                Group {
-                    Text("Week of pregnancy")
-                    TextField("Week of Pregnancy", text: $weekOfPregnancy)
-                        .frame(height: 40)
-                        .padding(.horizontal, 15)
-                        .padding(.vertical, 5)
-                        .background(Color.white)
-                        .cornerRadius(15)
-                        .shadow(color: .black.opacity(0.2), radius: 3)
-                        .frame(maxWidth: 350)
-                        .keyboardType(.numberPad)
-                }
-                
-                Group {
-                    Text("Due Date")
-                    DatePicker("Select due date", selection: $dueDate, displayedComponents: .date)
-                        .datePickerStyle(CompactDatePickerStyle())
-                        .frame(height: 40)
-                        .padding(.horizontal, 15)
-                        .padding(.vertical, 5)
-                        .background(Color.white)
-                        .cornerRadius(15)
-                        .shadow(color: .black.opacity(0.2), radius: 3)
-                        .frame(maxWidth: 350)
-                }
-                
-                Group {
-                    Text("Known Allergies")
-                    TextField("Allergies", text: $allergies)
-                        .frame(height: 40)
-                        .padding(.horizontal, 15)
-                        .padding(.vertical, 5)
-                        .background(Color.white)
-                        .cornerRadius(15)
-                        .shadow(color: .black.opacity(0.2), radius: 3)
-                        .frame(maxWidth: 350)
-                }
-            }
-            
-            Spacer()
-            
-            Button(action: {
-                currentPage += 1
-            }, label: {
-                Text("Let's Go!")
-                    .font(.body)
+                Text("Tell us who you are!")
+                    .font(.largeTitle)
                     .fontWeight(.semibold)
-                    .frame(maxWidth: .infinity)
-                    .padding()
-                    .foregroundColor(.white)
-                    .background(Color(uiColor: .preimary))
-                    .cornerRadius(10)
-                    .padding(.horizontal, 20)
-            })
-            
-            Spacer()
+                    .padding(.bottom, 30)
+                
+                VStack(alignment: .leading, spacing: 15) {
+                    Group {
+                        Text("Enter your full name")
+                        TextField("Name", text: $userName)
+                            .frame(height: 40)
+                            .padding(.horizontal, 15)
+                            .padding(.vertical, 5)
+                            .background(Color.white)
+                            .cornerRadius(15)
+                            .shadow(color: .black.opacity(0.2), radius: 3)
+                            .frame(maxWidth: 350)
+                    }
+                    
+                    Group {
+                        Text("Select your current stage")
+                        DropDownView(
+                            hint: "Select",
+                            options: [
+                                "Pre-Pregnant",
+                                "Currently Pregnant",
+                                "Post-Pregnant"
+                            ],
+                            anchor: .bottom,
+                            selection: $currentStage
+                        )
+                    }
+                    
+                    Group {
+                        Text("Week of pregnancy")
+                        TextField("Week of Pregnancy", text: $weekOfPregnancy)
+                            .frame(height: 40)
+                            .padding(.horizontal, 15)
+                            .padding(.vertical, 5)
+                            .background(Color.white)
+                            .cornerRadius(15)
+                            .shadow(color: .black.opacity(0.2), radius: 3)
+                            .frame(maxWidth: 350)
+                            .keyboardType(.numberPad)
+                    }
+                    
+                    Group {
+                        Text("Due Date")
+                        DatePicker("Select due date", selection: $dueDate, displayedComponents: .date)
+                            .datePickerStyle(CompactDatePickerStyle())
+                            .frame(height: 40)
+                            .padding(.horizontal, 15)
+                            .padding(.vertical, 5)
+                            .background(Color.white)
+                            .cornerRadius(15)
+                            .shadow(color: .black.opacity(0.2), radius: 3)
+                            .frame(maxWidth: 350)
+                    }
+                    
+                    Group {
+                        Text("Known Allergies")
+                        TextField("Allergies", text: $allergies)
+                            .frame(height: 40)
+                            .padding(.horizontal, 15)
+                            .padding(.vertical, 5)
+                            .background(Color.white)
+                            .cornerRadius(15)
+                            .shadow(color: .black.opacity(0.2), radius: 3)
+                            .frame(maxWidth: 350)
+                    }
+                }
+                
+                Spacer()
+                
+                Button(action: {
+                    currentPage += 1
+                }, label: {
+                    Text("Let's Go!")
+                        .font(.body)
+                        .fontWeight(.semibold)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(isFormComplete ? Color(uiColor: .preimary) : Color.gray)
+                        .cornerRadius(10)
+                })
+                .disabled(!isFormComplete)
+                
+                Spacer()
+            }
+            .padding()
+            .background(
+                Color.clear.contentShape(Rectangle())
+                    .onTapGesture {
+                        hideKeyboard()
+                    }
+            )
         }
     }
 }
+
 
 #Preview {
     EnterInfoView(currentPage: .constant(1))

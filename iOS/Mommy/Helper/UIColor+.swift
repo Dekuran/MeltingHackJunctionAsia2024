8c33ac25477fd8244f46ebb7c53068e128e22e42
Sae Nuruki <sae.webwork@gmail.com>
2024-08-10 16:47:09 +0900

    Create Home

--- iOS/Mommy/Helper/UIColor+.swift
+++ iOS/Mommy/Helper/UIColor+.swift
@@ -3,4 +3,5 @@ import UIKit
 extension UIColor {
     static let preimary: UIColor = .init(red: 241 / 255, green: 203 / 255, blue: 228 / 255, alpha: 1.0)
     static let secondary: UIColor = .init(red: 248 / 255, green: 234 / 255, blue: 243 / 255, alpha: 1.0)
+    static let button: UIColor = .init(red: 44 / 255, green: 44 / 255, blue: 44 / 255, alpha: 1.0)
 }
